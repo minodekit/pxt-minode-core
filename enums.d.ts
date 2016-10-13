9e9bd2fdc2995ebcf4f94353a3f1a825bbba85d5
Yongjia Niu <ahnniu@gmail.com>
2016-10-13 17:52:05 +0800

    Add enum defined in typescript, Other package based on this should use the enums

---
+++ enums.d.ts
@@ -0,0 +1,44 @@
+// Auto-generated. Do not edit.
+
+
+    declare enum ConnName
+    {
+    A0 = 0,
+    A1 = 1,
+    A2 = 2,
+    D12 = 12,
+    D13 = 13,
+    D14 = 14,
+    D15 = 15,
+    }
+
+
+    declare enum MINODE_CORE_IDS
+    {
+    MINODE_ID_START = 100,
+    MINODE_ID_NC = 199,
+
+    MINODE_ID_A0 = 100,
+    MINODE_ID_A1 = 102,
+    MINODE_ID_A2 = 104,
+
+    MINODE_ID_D_START = 110,
+    MINODE_ID_D0 = 199,
+    MINODE_ID_D1 = 199,
+    MINODE_ID_D2 = 199,
+    MINODE_ID_D3 = 199,
+    MINODE_ID_D4 = 199,
+    MINODE_ID_D5 = 199,
+    MINODE_ID_D6 = 199,
+    MINODE_ID_D7 = 199,
+    MINODE_ID_D8 = 199,
+    MINODE_ID_D9 = 199,
+    MINODE_ID_D10 = 199,
+    MINODE_ID_D11 = 199,
+    MINODE_ID_D12 = 134,
+    MINODE_ID_D13 = 136,
+    MINODE_ID_D14 = 138,
+    MINODE_ID_D15 = 140,
+    }
+
+// Auto-generated. Do not edit. Really.
